7eb7a306de3b34ab974ad075d5d2fdbe768052c0
AkshatRamanathan <ramanathan.ak@northeastern.edu>
2022-10-20 18:51:42 -0400

    main screen design prototype
    main screen basic wireframe

---
+++ src/model/Community.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author akshb
+ */
+class Community {
+
+    private String city;
+    private String country;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Community(String city, String country) {
+        this.city = city;
+        this.country = country;
+    }
+
+}
---
+++ src/model/Person.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author akshb
+ */
+public class Person {
+
+    private String name;
+    private String emailId;
+    private long phoneNumber;
+    private String address;
+    private long personId;
+    private Community community;
+
+    /**
+     *
+     * @param name
+     * @param emailId
+     * @param phoneNumber
+     * @param address
+     * @param personId
+     * @param community
+     */
+    public Person(String name, String emailId, long phoneNumber, String address, long personId, Community community) {
+        this.name = name;
+        this.emailId = emailId;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.personId = personId;
+        this.community = community;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(long personId) {
+        this.personId = personId;
+    }
+
+    public Community getCommunity() {
+        return community;
+    }
+
+    public void setCommunity(Community community) {
+        this.community = community;
+    }
+
+}
